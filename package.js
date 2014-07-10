fbee2ae8dbe6fd4f05a15d9923e597ba3c6ea947
Chris Hitchcott <hitchcott@gmail.com>
2014-07-10 11:15:22 +0800

    Indentation woes

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
-    summary: "A Bootstrap Theme Editor ported to Meteor"
+  summary: "A Bootstrap Theme Editor ported to Meteor"
 });
 
 Package.on_use(function(api) {
-    api.use([
-        'coffeescript',
-        'templating'
-    ], ['client']);
+  api.use([
+    'coffeescript',
+    'templating'
+  ], ['client']);
 
-    api.add_files('bootstrap-magic.js', ['client', 'server']);
+  api.add_files('bootstrap-magic.js', ['client', 'server']);
 });
